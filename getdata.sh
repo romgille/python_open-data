be04fcf3ec055af91bc7df5a871599a317ce2f2e
Romain Gille <romain.gille@gmail.com>
2016-04-08 09:28:19 +0200

    dat -> csv

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,5 @@
 # Récupérer les données à jour
 
-wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat -O data/airports.dat
-wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -O data/airlines.dat
-wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat -O data/routes.dat
+wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat -O data/airports.csv
+wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -O data/airlines.csv
+wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat -O data/routes.csv
